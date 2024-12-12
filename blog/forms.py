9cb428f7a0b8ab813bd1188587eafabb6bcb89a5
dylan <dylanwanjiku43@gmail.com>
2024-12-12 22:33:06 +0300

    files

---
+++ blog/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import PostModel, Comment
+
+
+class PostModelForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}))
+
+    class Meta:
+        model = PostModel
+        fields = ('title', 'content')
+
+
+class PostUpdateForm(forms.ModelForm):
+    class Meta:
+        model = PostModel
+        fields = ('title', 'content')
+        labels = {
+            'title': 'Title of the Innovation',
+            'content': 'Description of the Innovation',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        label='', widget=forms.TextInput(attrs={'placeholder': 'Add comment here....'}))
+
+    class Meta:
+        model = Comment
+        fields = ('content',)
